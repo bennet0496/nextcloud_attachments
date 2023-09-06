c44651d79d3cf124780296ec33c55e6f770ac0bd
Bennet Becker <bbecker@pks.mpg.de>
2023-09-06 14:29:57 +0200

    add License

--- nextcloud_attachments.php
+++ nextcloud_attachments.php
@@ -1,8 +1,23 @@
 <?php
-//define('RCUBE_CONFIG_DIR',  realpath(dirname(__FILE__)."/../../config"));
-//define('RCUBE_PLUGINS_DIR', realpath(dirname(__FILE__)."/../../plugins"));
+// Copyright (c) 2023 Bennet Becker <dev@bennet.cc>
 //
-//require_once dirname(__FILE__).'/vendor/framework/bootstrap.php';
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 include dirname(__FILE__)."/vendor/autoload.php";
 
