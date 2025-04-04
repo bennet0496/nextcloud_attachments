dedbfaa411b68cb901f400a435e01771023f8b80
Bennet Becker <bbecker@pks.mpg.de>
2025-04-04 16:03:20 +0200

    delete redundant password template

--- nextcloud_attachments.php
+++ nextcloud_attachments.php
@@ -95,6 +95,7 @@ class nextcloud_attachments extends rcube_plugin
         //insert our client script and style
         $this->add_hook("ready", function ($param) { $this->insert_client_code($param); });
 
+        //TODO actually implement the following
         $this->add_hook("render_page", function ($param) {
             if($param["template"] == "compose") {
                 $this->rcmail->output->add_footer(file_get_contents($this->home . "/templates/inline.html"));
